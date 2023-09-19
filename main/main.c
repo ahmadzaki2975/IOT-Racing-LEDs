6b505a9ccd95a985490e0b4b4f5e97193e117c8c
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-09-19 17:31:27 +0700

    feat: can now change the period of LED 1

--- main/main.c
+++ main/main.c
@@ -1,28 +1,71 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "rom/gpio.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "ctype.h"
 
+// Queue handle
+QueueHandle_t uartQueue;
+QueueHandle_t ledQueue;
+
+// Definisi PIN UART
+#define TX_PIN 1
+#define RX_PIN 3
+
+// Definisi PIN LED
 #define LED1 13
 #define LED2 12
 #define LED3 14
 #define LED4 27
 
+void shiftString(char *str) {
+  int len = strlen(str);
+  for (int i = 0; i < len; i++) {
+    str[i] = str[i+1];
+  }
+  str[len] = '\0';
+}
+
+void emptyString(char *str)
+{
+  int len = strlen(str);
+  for (int i = 0; i < len; i++)
+  {
+    str[i] = '\0';
+  }
+}
+
+// Blink untuk LED 1
 void BlinkLED1(void *pvParameters)
 {
   (void)pvParameters;
   gpio_pad_select_gpio(LED1);
   gpio_set_direction(LED1, GPIO_MODE_OUTPUT);
+  uint16_t period = 100;
   while (1)
   {
+    char queueData[64];
+    if (xQueueReceive(ledQueue, &queueData, pdMS_TO_TICKS(100)) == pdPASS && period > 0)
+    {
+      if (queueData[0] == '1')
+      {
+        shiftString(queueData);
+        period = atoi(queueData);
+        printf("Period of LED 1 changed to: %i\n", period);
+      }
+    }
     gpio_set_level(LED1, 0);
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(period / portTICK_PERIOD_MS);
     gpio_set_level(LED1, 1);
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(period / portTICK_PERIOD_MS);
   }
 }
 
+// Blink untuk LED 2
 void BlinkLED2(void *pvParameters)
 {
   (void)pvParameters;
@@ -31,12 +74,13 @@ void BlinkLED2(void *pvParameters)
   while (1)
   {
     gpio_set_level(LED2, 0);
-    vTaskDelay(150 / portTICK_PERIOD_MS);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
     gpio_set_level(LED2, 1);
-    vTaskDelay(150 / portTICK_PERIOD_MS);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
   }
 }
 
+// Blink untuk LED 3
 void BlinkLED3(void *pvParameters)
 {
   (void)pvParameters;
@@ -45,12 +89,13 @@ void BlinkLED3(void *pvParameters)
   while (1)
   {
     gpio_set_level(LED3, 0);
-    vTaskDelay(200 / portTICK_PERIOD_MS);
+    vTaskDelay(300 / portTICK_PERIOD_MS);
     gpio_set_level(LED3, 1);
-    vTaskDelay(200 / portTICK_PERIOD_MS);
+    vTaskDelay(300 / portTICK_PERIOD_MS);
   }
 }
 
+// Blink untuk LED 4
 void BlinkLED4(void *pvParameters)
 {
   (void)pvParameters;
@@ -59,16 +104,69 @@ void BlinkLED4(void *pvParameters)
   while (1)
   {
     gpio_set_level(LED4, 0);
-    vTaskDelay(250 / portTICK_PERIOD_MS);
+    vTaskDelay(400 / portTICK_PERIOD_MS);
     gpio_set_level(LED4, 1);
-    vTaskDelay(250 / portTICK_PERIOD_MS);
+    vTaskDelay(400 / portTICK_PERIOD_MS);
+  }
+}
+
+char received_message[64];
+int message_index = 0;
+int selected_led = 0;
+// Read data dari UART
+void readUART()
+{
+  while (1)
+  {
+    char data;
+    uart_read_bytes(UART_NUM_0, &data, 1, portMAX_DELAY);
+    printf("%c\n", data);
+    if (data == '1' && selected_led == 0)
+    {
+      selected_led = 1;
+      printf("Changing Period of LED 1\n");
+    }
+    if (selected_led == 1)
+    {
+      if (data == ' ')
+      {
+        received_message[message_index] = '\0';
+        xQueueSend(ledQueue, received_message, portMAX_DELAY);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        message_index = 0;
+        selected_led = 0;
+        emptyString(received_message);
+        continue;
+      }
+      received_message[message_index] = data;
+      message_index++;
+    }
   }
 }
 
 void app_main(void)
 {
+  // Konfigurasi UART
+  const uart_config_t uart_config = {
+      .baud_rate = 115200,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
+  uart_param_config(UART_NUM_0, &uart_config);
+  uart_set_pin(UART_NUM_0, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+  uart_driver_install(UART_NUM_0, 256, 0, 0, NULL, 0);
+
+  // Membuat Queue untuk UART dan LED
+  uartQueue = xQueueCreate(10, sizeof(char[64]));
+  ledQueue = xQueueCreate(10, sizeof(char[64]));
+
+  // Membuat Task untuk Blink LED 1 hingga 4
   xTaskCreate(&BlinkLED1, "BlinkLED1", 2048, NULL, 5, NULL);
   xTaskCreate(&BlinkLED2, "BlinkLED2", 2048, NULL, 5, NULL);
   xTaskCreate(&BlinkLED3, "BlinkLED3", 2048, NULL, 5, NULL);
   xTaskCreate(&BlinkLED4, "BlinkLED4", 2048, NULL, 5, NULL);
+
+  // Membuat Task untuk membaca data dari UART
+  xTaskCreate(&readUART, "readUART", 2048, NULL, 5, NULL);
 }
